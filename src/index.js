9552a46e6ee32e48ccc2993bc9c93724e79f5598
Aniya Khawaja <aniya.khawaja08@gmail.com>
2024-07-21 15:53:43 +0300

    edited js file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 function updateWeather(response) {
     let tempElement = document.querySelector("#temp");
-    let temp = response.data.temp.current;
+    let temp = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    
-    cityElement.innerHTML = searchInput.value;
+  
+    cityElement.innerHTML = response.data.city;
     tempElement.innerHTML = Math.round(temp);
-}
-
-function searchCity (city) {
-let apiKey = "69ba4a782t23bf4e605dfe17d2ob01a9";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${69ba4a782t23bf4e605dfe17d2ob01a9}&units=metric`;
- axios.get(apiUrl).then(updateWeather);
-}
-
-function handleSearchSubmit(event) {
-event.preventDefault();
-let searchInput = document.querySelector("#search-form-input");
-
-searchCity(searchInput.value);
-}
-
-et searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Italy");
+  }
+  
+  function searchCity(city) {
+    let apiKey = "69ba4a782t23bf4e605dfe17d2ob01a9";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+  }
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+  
+    searchCity(searchInput.value);
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  searchCity("Paris");
+  
+  
+  
+  
+  
