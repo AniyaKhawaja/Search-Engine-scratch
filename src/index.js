a474f37dc90db289f953d71355ffb9f883b588ad
Aniya Khawaja <aniya.khawaja08@gmail.com>
2024-07-22 19:24:36 +0300

    updated js file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,40 @@ function updateWeather(response) {
     let tempElement = document.querySelector("#temp-value");
     let temp = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = newDate(date.getDay);
   
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
     tempElement.innerHTML = Math.round(temp);
+  }
+
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+       "Wednesday", 
+       "Thursday",
+       "Friday", 
+       "Saturday",
+       
+      ];
+      let day = days[date.getDay()];
+
+      if(minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+
+      return `${day} ${hours}:${minutes}`;
   }
   
   function searchCity(city) {
